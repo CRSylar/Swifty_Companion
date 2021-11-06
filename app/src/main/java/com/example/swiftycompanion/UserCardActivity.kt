b7747f6cd6480ddb48a5407b6285e7b99c362aac
CRSylar <cristianoromaldetti@icloud.com>
2021-11-06 12:22:20 +0100

    aggiuta foto e userCard, ongoing

--- app/src/main/java/com/example/swiftycompanion/UserCardActivity.kt
+++ app/src/main/java/com/example/swiftycompanion/UserCardActivity.kt
@@ -1,5 +1,7 @@
 package com.example.swiftycompanion
 
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +9,8 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
+import com.example.swiftycompanion.databinding.ActivityUserCardBinding
 import org.json.JSONObject
 
 var g_userData: Map<String, *> ? = null
@@ -14,19 +18,24 @@ var listen = MutableLiveData<Boolean>(false)
 
 class UserCardActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityUserCardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_card)
+        binding = ActivityUserCardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val textView = findViewById<TextView>(R.id.userDataText)
         listen.observe(this, Observer {
             if (listen.value == true) {
-                if (g_userData == null)
-                {
+                if (g_userData == null) {
                     Toast.makeText(this, "User Not Found!", Toast.LENGTH_LONG).show()
                     finish()
                 }
-                textView.text = g_userData.toString()
+                Glide
+                    .with(this)
+                    .load(g_userData!!["image_url"])
+                    .into(binding.proPic)
+                binding.userDataText.text = g_userData.toString()
             }
         })
     }
