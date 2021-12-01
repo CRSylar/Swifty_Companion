b691b0361f62b582d3cc907c2f7764bc0ac20ae1
Cristiano Romaldetti <74542458+CRSylar@users.noreply.github.com>
2021-12-01 17:39:15 +0100

    Update private.kt

--- app/src/main/java/com/example/swiftycompanion/private.kt
+++ app/src/main/java/com/example/swiftycompanion/private.kt
@@ -1,7 +1,7 @@
 package com.example.swiftycompanion
 
-const val client_id: String = "5b3ab3bacdf8b5f4ef716b3b757a2512fdbb3b0242a5c7a447891013e40c100b"
-const val secret: String = "e249c191bf0551c1c5c0ff0b2e54181d50045d56941cc21d31fcf27932c00737"
+const val client_id: String = "5b3ab3bacdf8b5f4ef716b3b757a2512fdbb3b0242a5c7a447891013e40c100b" #Revoked
+const val secret: String = "e249c191bf0551c1c5c0ff0b2e54181d50045d56941cc21d31fcf27932c00737" #Revoked
 const val uri : String = "https://api.intra.42.fr/oauth/token"
 const val searchUri: String = "https://api.intra.42.fr/v2/users/"
 const val header: String = "Bearer "
